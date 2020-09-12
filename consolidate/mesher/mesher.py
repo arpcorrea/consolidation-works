14ba584022464d76fb3fbdabbf1252cbc48dae42
michael <michael.b@clicktech.com>
2020-09-12 18:36:55 +0100

    Moved self.fields and self.meshes to __init__ attribute.

--- consolidate/mesher/mesher.py
+++ consolidate/mesher/mesher.py
@@ -3,20 +3,21 @@ import numpy as np
 from .mesh import Mesh
 from .fields import Field
 
+
 class Mesher():
 
-    def __init__(self,  problem):
+    def __init__(self, problem):
+        self.fields = []
+        self.meshes = []
         self.totalNx = problem.totalNx
         self.totalNy = problem.totalNy
         self.create_masks(problem)
         self.set_intial_condition_fields(problem)
 
     def create_masks(self, problem):
-        self.meshes=[]
         for domain in problem.domains:
-            self.meshes.append(Mesh( domain, self.totalNx, self.totalNy))
+            self.meshes.append(Mesh(domain, self.totalNx, self.totalNy))
 
     def set_intial_condition_fields(self, problem):
-        self.fields=[]
         for fields in problem.required_fields:
             self.fields.append(Field(fields, problem))
