7b5b0c38510424f513df7e2fe3b2131f16f03504
michael <michael.b@clicktech.com>
2020-09-17 17:08:06 +0100

    Reformatted file to ensure line length was no more than 100 characters.

--- consolidate/problems/oneplate.py
+++ consolidate/problems/oneplate.py
@@ -1,6 +1,7 @@
 from .domains import RectangularDomain
 from .boundaryconditions import LinearBC
 
+
 class OnePlate:
 
     def __init__(self, deck):
@@ -15,19 +16,23 @@ class OnePlate:
 
     def set_domains(self, deck):
         self.domains = []
-        corner0 = (0,0)
-        corner1 = (self.Lx ,self.Ly)
+        corner0 = (0, 0)
+        corner1 = (self.Lx, self.Ly)
         plate_material = deck.doc["Materials"]["Plate"]
-        self.domains.append(RectangularDomain( "Plate", corner0, corner1, plate_material))
+        self.domains.append(RectangularDomain("Plate", corner0, corner1, plate_material))
 
     def set_boundaryconds(self, deck):
         self.boundaryconditions = []
         for deck_BC in deck.doc["Boundary Conditions"]:
             if deck_BC == "Top":
-                self.boundaryconditions.append( LinearBC( (0.,self.Ly), (self.Lx,self.Ly), deck.doc["Boundary Conditions"][deck_BC] ) )
+                self.boundaryconditions.append(LinearBC((0., self.Ly), (self.Lx, self.Ly),
+                                                        deck.doc["Boundary Conditions"][deck_BC]))
             elif deck_BC == "Bottom":
-                self.boundaryconditions.append( LinearBC( (0.,0.), (self.Lx, 0.), deck.doc["Boundary Conditions"][deck_BC] ) )
+                self.boundaryconditions.append(
+                    LinearBC((0., 0.), (self.Lx, 0.), deck.doc["Boundary Conditions"][deck_BC]))
             elif deck_BC == "Left":
-                self.boundaryconditions.append( LinearBC( (0.,0.), (0.,self.Ly), deck.doc["Boundary Conditions"][deck_BC] ) )
+                self.boundaryconditions.append(
+                    LinearBC((0., 0.), (0., self.Ly), deck.doc["Boundary Conditions"][deck_BC]))
             elif deck_BC == "Right":
-                self.boundaryconditions.append( LinearBC( (self.Lx,0.), (self.Lx,self.Ly), deck.doc["Boundary Conditions"][deck_BC] ) )
+                self.boundaryconditions.append(LinearBC((self.Lx, 0.), (self.Lx, self.Ly),
+                                                        deck.doc["Boundary Conditions"][deck_BC]))
